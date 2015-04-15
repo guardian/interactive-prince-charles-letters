181a9162f364e65b5e25e8a017e2179b73e34f5d
feilding <feilding.cage@gmail.com>
2015-04-15 16:54:45 +0100

    adds doc implementation, layout

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ define([
     'rvc!templates/navTemplate',
     'rvc!templates/LetterTemplate',
     'rvc!templates/twitterTemplate',
+    'json!data/letters.json'
 ], function(
     Ractive,
     get,
@@ -13,14 +14,15 @@ define([
     ShareTemplate,
     NavTemplate,
     LetterTemplate,
-    TwitterTemplate
+    TwitterTemplate,
+    data
 ) {
    'use strict';
 
     function init(el, context, config, mediator) {
         // DEBUG: What we get given on boot
-        console.log(el, context, config, mediator);
-
+        //console.log(el, context, config, mediator);
+        console.log(data)
         var base = new AppTemplate({
             el: el,
             components: {
@@ -35,24 +37,28 @@ define([
             base.find('#letter-' + id).scrollIntoView();
         })
 
-        var SPREADSHEET_KEY = '1wtPiBdw2T5VGOIpWsXWxPsnCDrsE3hAsJ2H9LdG-G0A';
-        get('http://interactive.guim.co.uk/spreadsheetdata/'+SPREADSHEET_KEY+'.json')
-            .then(JSON.parse)
-            .then(function(json){
+        base.set('config', data.header);       
+        base.set('letters', Object.keys(data.letters).map(function(k) { return data.letters[k] }) );
+        
+
+        // var SPREADSHEET_KEY = '1wtPiBdw2T5VGOIpWsXWxPsnCDrsE3hAsJ2H9LdG-G0A';
+        // get('http://interactive.guim.co.uk/spreadsheetdata/'+SPREADSHEET_KEY+'.json')
+        //     .then(JSON.parse)
+        //     .then(function(json){
 
                 
-                base.set('config', json.sheets.config[0]);       
-                base.set('letters', json.sheets.letters);
-                base.set('annotations', json.sheets.annotations);
+        //         base.set('config', json.sheets.config[0]);       
+        //         base.set('letters', json.sheets.letters);
+        //         base.set('annotations', json.sheets.annotations);
                 
-                //parse url for deep linking
-                if(window.location.hash != ''){
-                    var id = window.location.hash.replace('#letter', '');
-                    base.find('#letter-' + id).scrollIntoView();
-                }
+        //         //parse url for deep linking
+        //         if(window.location.hash != ''){
+        //             var id = window.location.hash.replace('#letter', '');
+        //             base.find('#letter-' + id).scrollIntoView();
+        //         }
 
 
-            });
+        //     });
 
     }
 
