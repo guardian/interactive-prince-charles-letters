4a38b2869c23c4cc236ac06d50de0fb524ae0fb4
feilding <feilding.cage@gmail.com>
2015-05-10 19:21:22 +0100

    sunday start

--- src/js/main.js
+++ src/js/main.js
@@ -21,29 +21,25 @@ define([
         // DEBUG: What we get given on boot
         //console.log(el, context, config, mediator);
 
-        var base = new AppTemplate({
-            el: el,
-            components: {
-                shareTemplate: ShareTemplate,
-                navTemplate: NavTemplate,
-                letterTemplate: LetterTemplate,
-                twitterTemplate: TwitterTemplate
-            }
-        })
-
-        base.on('navTemplate.scrollTo', function(e, id){
-            console.log()
-            base.find('#letter-' + id).scrollIntoView();
-        })
-
-        //base.set('config', data.header);       
-        
-        
-
 
         get('http://interactive.guim.co.uk/docsdata-test/1TexIb-OqXWcSEWG2uUJ0hx1b3RJyndXDfDjus0uaRsY.json')
             .then(JSON.parse)
             .then(function(json){
+
+                 var base = new AppTemplate({
+                    el: el,
+                    components: {
+                        shareTemplate: ShareTemplate,
+                        navTemplate: NavTemplate,
+                        letterTemplate: LetterTemplate,
+                        twitterTemplate: TwitterTemplate
+                    }
+                })
+
+                base.on('navTemplate.scrollTo', function(e, id){
+                    console.log()
+                    base.find('#letter-' + id).scrollIntoView();
+                })
                 
                 base.set('config', json.header);  
                 base.set('letters', Object.keys(json.letters).map(function(k) { return json.letters[k] }) );
