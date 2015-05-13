c6ab4aa6edddb3e1248f2503e4eaf7088d18c50d
feilding <feilding.cage@gmail.com>
2015-05-13 12:05:47 +0100

    switches to new doc

--- src/js/main.js
+++ src/js/main.js
@@ -24,9 +24,9 @@ define([
     function init(el, context, config, mediator) {
         // DEBUG: What we get given on boot
         //console.log(el, context, config, mediator);
-
-
-        get('http://interactive.guim.co.uk/docsdata-test/1TexIb-OqXWcSEWG2uUJ0hx1b3RJyndXDfDjus0uaRsY.json')
+        //http://interactive.guim.co.uk/docsdata-test/1TexIb-OqXWcSEWG2uUJ0hx1b3RJyndXDfDjus0uaRsY.json'
+        var url = 'http://interactive.guim.co.uk/docsdata-test/1H2Vw_WkKACcvsF3A1fgfmAqIoVGNNoTwPzYK7vWMcnc.json'
+        get(url)
             .then(JSON.parse)
             .then(function(json){
 
